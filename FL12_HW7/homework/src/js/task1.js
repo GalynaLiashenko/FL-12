07e1f8122140fe51bb6771efd19d6ce74c821568
Galyna Liashenko <58170038+GalynaLiashenko@users.noreply.github.com>
2019-12-29 13:34:46 +0200

    Add files via upload

---
+++ FL12_HW7/homework/src/js/task1.js
@@ -0,0 +1,47 @@
+const em = 5;
+const pas = 6;
+let b;
+let a = prompt('Email');
+console.log(a);
+if (!a) {
+    alert('Canceled')
+
+} else if (a.length < em) {
+ alert('I dont know any emails having name length less than 5 symbols'); 
+} else if (a === 'admin@gmail.com' || a === 'user@gmail.com') {
+    b = prompt('Password');
+    console.log(b);
+    if (b === '') {
+        alert('Canceled.')
+    }
+}
+if (a === 'admin@gmail.com' && b === 'AdminPass' || a === 'user@gmail.com' && b === 'UserPass') {
+    if (confirm('Do you want to change your password ?')) {
+
+        let d = prompt('Old password');
+        //  alert(d);
+        if (d === '') {
+            alert('Canceled.')
+        } else if (d.length < pas) {
+ alert('I dont know any emails having name length less than 6 symbols'); 
+} else if (d === b) {
+            let n = prompt('New password')
+            if (n === '') {
+                alert('Canceled.')
+            } else if (n.length < pas) {
+ alert('Its too short password. Sorry.'); 
+} else {
+              let m = prompt('type again')
+                if (m === n) {
+ alert('You have successfully changed your password') 
+}
+                if (m !== n) {
+ alert('You wrote the wrong password.') 
+}
+            }
+        }
+
+    } else {
+        alert('You have failed the change')
+    }
+}
---
+++ FL12_HW7/homework/src/js/task2.js
@@ -0,0 +1,54 @@
+let prize_sum = 0; let n = 5; let prize = 100; let nn = 4; let pr = 100; let pr2 = 2; let n2 = 5;
+let x4 = 4; let x2 = 2; let y1 = 1; let y2 = 2;
+if (confirm('Do you want to play a game?')) {
+    do {
+        n = n + nn;
+        let x = 1;
+        let y = 0;
+        console.log(n);
+        console.log('pr1' + prize);
+        let i = 3;
+        let a = Math.floor(Math.random() * n);
+        console.log('a'+a);
+        while (i > 0) {
+            console.log('x==' + x);
+            let p = n - 1;
+            let us = prompt('Choose a roulette pocket number from 0 to ' + Number(p)
+                + 'Attempts left:' + i + 'Total prize:' + prize_sum + '$'
+                + 'Possible prize for current attempt:' + prize);
+            console.log('us'+us)
+            if (us === null) {
+ break; 
+}
+            if (us === '') {
+ n = n2; prize = pr; break; 
+} else if (Number(us) === a) {
+                prize_sum = prize_sum + prize;
+                prize = prize * pr2 * x;
+                alert('Congratulation, you won!   Your prize is:' + prize_sum + '$'); break;
+            } else if (Number(us) !== a) {
+                i--;
+                y++; prize = prize / pr2;
+
+                if (x === x4) {
+                    x = 1;
+                }
+                if (y === 1) {
+
+                    x = x * x2;
+                }
+
+                if (y === y2) {
+
+                    x = x * x2;
+                }
+                console.log('x=' + x + 'y==' + y);
+            } else {
+                prize = pr; 
+}
+        }
+    }
+    while (confirm('Do you want to play again? '))
+} else {
+ alert('You did not become a billionaire, but can.') 
+}
